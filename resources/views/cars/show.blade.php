a674040a8d557394fcdbc2e0e14bcf5c702c2608
Adrians <adrians@Opti-06.localdomain>
2026-04-22 13:55:15 +0300

    Added some design

--- resources/views/cars/show.blade.php
+++ resources/views/cars/show.blade.php
@@ -1,5 +1,11 @@
 <x-app-layout>
-    <h1>{{$data['name']}}</h1>
-    <p>Year: {{$data['year']}}</p>
-    <p>Total milage: {{$data['totalDistance']}}</p>
+    <ul>
+        <li>
+        <h1>{{$data['name']}}</h1>
+        <p>Year: {{$data['year']}}</p>
+        <p>Total milage: {{$data['totalDistance']}}</p>
+        </li>
+    </ul>
+
+
 </x-app-layout>
